82248409cd862c19c47e2b376dbf8a2433bc3aaf
unknown <mittalshubham05@gmail.com>
2020-10-03 18:27:48 +0530

    forEach using proper and anonymous classes

--- lambdaExpression/src/main/java/bridgelabz/lambdaExpression/MainClass.java
+++ lambdaExpression/src/main/java/bridgelabz/lambdaExpression/MainClass.java
@@ -1,21 +1,51 @@
 package bridgelabz.lambdaExpression;
 
-import bridgelabz.lambdaExpression.LambdaExpressions.MathFunctions;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import bridgelabz.lambdaExpression.MathFunctions;
 
 /**
  * Hello world!
+ *
+ */
+/**
+ * @author Shubham, MainClass for controlling execution
  *
  */
 public class MainClass 
 {
     public static void main( String[] args )
     {
-    	MathFunctions add = (x,y) -> (x+y);
-    	MathFunctions subtract = (x,y) -> (x-y);
-    	MathFunctions multiply = (x,y) -> (x*y);
     	
-    	MathFunctions.printExpression(10, 20, "Addition", add);
-    	MathFunctions.printExpression(10, 20, "Subtraction", subtract);
-    	MathFunctions.printExpression(10, 20, "Multiplication", multiply);
+    	List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+    	
+    	/**
+    	 * Printing through proper class
+    	 */
+    	CustomConsumer action = new CustomConsumer();
+    	numbers.forEach(action);
+    	
+    	
+    	/**
+    	 * Printing through Anonymous class creation
+    	 */
+    	numbers.forEach(new Consumer<Integer>() {
+    		@Override
+    		public void accept(Integer t) {
+    			System.out.println("The number through anonymous class " + t );
+    		};
+		});
+    	
+    	
+    	/**
+    	 * Printing using lambda function
+    	 */
+    	Consumer<Integer> consumerObj = t -> {System.out.println("The number through "
+    										+ "lambda function " + t);
+    										 };
+    	numbers.forEach(consumerObj);
     }
 }
+
